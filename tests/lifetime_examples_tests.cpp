226fe8d3bb70862e1510a5fe013fbe573c5ef71f
Jørgen Fogh <jrgfogh@gmail.com>
2024-12-15 23:20:01 +0100

    Added BorrowedData example.

--- tests/lifetime_examples_tests.cpp
+++ tests/lifetime_examples_tests.cpp
@@ -41,6 +41,17 @@ TEST(lifetime_examples_tests, SharedOwnership) {
 			std::cout << "Input A: " << *inputA << std::endl;
 			std::cout << "Input B: " << *inputB << std::endl;
 		}, std::make_shared<int>(16));
-	std::cout << "Input B: " << *inputA << std::endl;
+	std::cout << "Input A: " << *inputA << std::endl;
+	fut.get();
+}
+
+TEST(lifetime_examples_tests, BorrowedData) {
+	auto inputA = 25;
+	auto inputB = 16;
+	auto fut = std::async([&inputA](int &inputB) {
+			std::cout << "Input A: " << inputA << std::endl;
+			std::cout << "Input B: " << inputB << std::endl;
+		}, std::ref(inputB));
+	std::cout << "Input B: " << inputA << std::endl;
 	fut.get();
 }
