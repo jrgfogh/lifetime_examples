d469b426fdb2068a6d9a91ef650d0be45e6a8731
Jørgen Fogh <jrgfogh@gmail.com>
2024-12-15 22:58:09 +0100

    Added build scripts

---
+++ tests/lifetime_examples_tests.cpp
@@ -0,0 +1,13 @@
+#pragma warning(push)
+#pragma warning(disable : 26495)
+#pragma warning(disable : 26812)
+#pragma warning(disable : 26439)
+
+#include <gtest/gtest.h>
+
+#pragma warning(pop)
+
+
+TEST(lifetime_examples_tests, Foobar) {
+	ASSERT_EQ(1, 1);
+}
