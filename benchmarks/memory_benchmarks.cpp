45660bdf996d94ea9706a1c39613cf57df2b7cbd
Jørgen Fogh <jrgfogh@gmail.com>
2024-12-25 21:56:45 +0100

    More memory benchmarks.

--- benchmarks/memory_benchmarks.cpp
+++ benchmarks/memory_benchmarks.cpp
@@ -1,33 +1,14 @@
 #include <benchmark/benchmark.h>
 
-#include <vector>
+#include <barrier>
+#include <memory>
 #include <thread>
+#include <vector>
 
 constexpr size_t GB = 1 << 30;
 constexpr size_t elems = GB / (sizeof std::vector<int> + 2 * sizeof(int));
 
-static void BM_AllocateOneThread(benchmark::State& state) {
-    for (auto _ : state) {
-        // We want to time only the allocation, not the deallocation.
-        std::vector<std::vector<int>> v{elems, {5, 7}};
-        state.PauseTiming();
-    }
-}
-
-// BENCHMARK(BM_AllocateOneThread)->Repetitions(10)->UseRealTime();
-
-static void BM_FreeOneThread(benchmark::State& state) {
-    for (auto _ : state) {
-        state.PauseTiming();
-        std::vector<std::vector<int>> v{elems, {5, 7}};
-        state.ResumeTiming();
-        // We want to time the deallocation, which happens implicitly here.
-    }
-}
-
-// BENCHMARK(BM_FreeOneThread)->Repetitions(10)->UseRealTime();
-
-static void BM_AllocateManyThreads(benchmark::State& state) {
+static void BM_Allocate(benchmark::State& state) {
     for (auto _ : state) {
         // We want to time only the allocation, not the deallocation.
         std::vector<std::vector<int>> v{elems / state.threads(), {5, 7}};
@@ -35,9 +16,9 @@ static void BM_AllocateManyThreads(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_AllocateManyThreads)->Repetitions(10)->ThreadRange(1, 8)->UseRealTime();
+BENCHMARK(BM_Allocate)->Repetitions(10)->ThreadRange(1, 8)->UseRealTime()->MeasureProcessCPUTime();
 
-static void BM_FreeManyThreads(benchmark::State& state) {
+static void BM_FreeSameThread(benchmark::State& state) {
     for (auto _ : state) {
         state.PauseTiming();
         std::vector<std::vector<int>> v{elems / state.threads(), {5, 7}};
@@ -46,10 +27,39 @@ static void BM_FreeManyThreads(benchmark::State& state) {
     }
 }
 
-BENCHMARK(BM_FreeManyThreads)->Repetitions(10)->ThreadRange(1, 8)->UseRealTime();
+BENCHMARK(BM_FreeSameThread)->Repetitions(10)->ThreadRange(1, 8)->UseRealTime()->MeasureProcessCPUTime();
+
+struct shared_state {
+    explicit shared_state(size_t size) :
+        vs{size},
+        barrier{static_cast<ptrdiff_t>(size)} {}
+
+    std::vector<std::vector<std::vector<int>>> vs;
+    std::barrier<> barrier;
+};
+
+static void BM_FreeOtherThread(benchmark::State& state) {
+    static std::unique_ptr<shared_state> shared;
+    if (state.thread_index() == 0) {
+        shared = std::make_unique<shared_state>(state.threads());
+    }
+    auto index = state.thread_index();
+    for (auto _ : state) {
+        auto &vs = shared->vs;
+        auto &barrier = shared->barrier;
+        state.PauseTiming();
+        vs[index] = {elems / state.threads(), {5, 7}};
+
+        barrier.arrive_and_wait();
+        state.ResumeTiming();
+
+        index = (index + 1) % state.threads();
+        vs[index].resize(0);
+        vs[index].shrink_to_fit();
+    }
+}
+
+BENCHMARK(BM_FreeOtherThread)->Repetitions(10)->ThreadRange(1, 16)->UseRealTime()->MeasureProcessCPUTime();
 
 // TODO: Missing benchmarks:
 // 1. Shuffled data.
-// 2. Several threads:
-//   i) No cooperation +
-//   ii) A allocates, B frees
